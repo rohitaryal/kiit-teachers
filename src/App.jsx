255b055dd8a223d5ae1b77361a79c5068f8aa8b2
rohitaryal <erucix@gmail.com>
2024-07-15 00:17:21 +0530

    Added files

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
+import "./App.css";
+
+const App = () => {
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData()
+      .then(data => {
+        setData(data);
+        setDataLoaded(true);
+      })
+  }, []);
+
+  if (dataLoaded) {
+    return <Card json={data} />
+  } else {
+    return "LOADING"
+  }
+}
+
+const getData = async () => {
+  return fetch("./list.json")
+    .then(data => data.json())
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,53 @@
+const Card = ({ json }) => {
+    return (
+        <>
+            <div className="card-container">
+                {
+                    json.map(teacher => {
+                        return (
+                            <div className="card" key={crypto.randomUUID()}>
+                                <div className="user-profile" style={{
+                                    backgroundImage: `url(${teacher.profileUrl})`
+                                }}></div>
+                                <div className="user-detail-holder">
+                                    <div className="user-name">{teacher.name}</div>
+                                    <div className="job-title">{teacher.jobTitle}</div>
+                                    <div className="user-description">
+                                        {teacher.description}
+                                    </div>
+                                    <div className="user-stats">
+                                        <div className="likes">
+                                            <div className="like-count">{teacher.likes}</div>
+                                            <div className="mdi">Likes</div>
+                                        </div>
+                                        <div className="dislikes">
+                                            <div className="dislike-count">{teacher.dislikes}</div>
+                                            <span className="mdi">Dislikes</span>
+                                        </div>
+                                        <div className="sections">
+                                            <div className="section-count">{teacher.sections.join(", ")}</div>
+                                            <span className="mdi">Classroom</span>
+                                        </div>
+                                    </div>
+                                    <ul className="subjects">
+                                        {
+                                            teacher.subjects.map(sub => {
+                                                return <li>{sub}</li>
+                                            })
+                                        }
+                                    </ul>
+                                    <div className="buttons">
+                                        <button onClick={() => window.open("", "_blank")}>Details</button>
+                                        <button onClick={() => window.open(teacher.moreInfo, "_blank")}>View Profile</button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
+}
+
+export default Card;
